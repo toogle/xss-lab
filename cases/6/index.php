a3f148f09fc06ae37767e14871e2b7f967616fba
psfinaki <holeheader@gmail.com>
2015-04-01 19:30:18 +0300

    Cats are working (very roughly so far)

--- cases/6/index.php
+++ cases/6/index.php
@@ -4,7 +4,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title>Задание №3 / Лабораторная работа №2. XSS</title>
+		<title>Задание №6 / Лабораторная работа №2. XSS</title>
 		<meta name="description" content="XSS Lab">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -47,7 +47,7 @@
 							<ul class="nav nav-pills nav-stacked nav-inner">
 								<li><a href="../1/">Задание №1</a></li>
 								<li><a href="../2/">Задание №2</a></li>
-								<li><a href="../3/">Задание №3</a></li>
+								<li><a href="../3/">Задание №3</a></li>								
 								<li><a href="../4/">Задание №4</a></li>
 								<li class="active"><a href=".">Задание №6</a></li>
 							</ul>
@@ -55,9 +55,45 @@
 						<li><a href="https://github.com/toogle/xss-lab" target="_blank">Исходный код</a></li>
 					</ul>
 				</div><!-- .col-md-3 -->
+
+				<div class="col-md-9">
+					<div class="well well-lg">
+						<div id="myCarousel" class="carousel slide" data-ride="carousel">
+							<!-- Indicators -->
+							<ol class="carousel-indicators">
+							    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+							    <li data-target="#myCarousel" data-slide-to="1"></li>
+							    <li data-target="#myCarousel" data-slide-to="2"></li>
+							    <li data-target="#myCarousel" data-slide-to="3"></li>
+							</ol>
+
+							<?php echo MEDIA_URL; ?>
+
+							  <!-- Wrapper for slides -->
+							<div class="carousel-inner" role="listbox">
+							    <div class="item active">
+							        <img src="<?php echo MEDIA_URL; ?>/cases/6/cats/1.jpg" alt="Chania">
+							    </div>
+
+							    <div class="item">
+							      	<img src="<?php echo MEDIA_URL; ?>/cases/6/cats/2.jpg" alt="Chania">
+							    </div>
+
+							    <div class="item">
+							      	<img src="<?php echo MEDIA_URL; ?>/cases/6/cats/3.jpg" alt="Flower">
+							    </div>
+
+							    <div class="item">
+							      	<img src="<?php echo MEDIA_URL; ?>/cases/6/cats/4.jpg" alt="Flower">
+							    </div>
+							</div>
+						</div>
+					</div><!-- .well -->
+				</div><!-- .col-md-9 -->
 			</div><!-- .row -->
 		</div><!-- .container -->
 
 		<script src="<?php echo MEDIA_URL; ?>/js/lib/jquery-1.11.1.min.js"></script>
+		<script src="<?php echo MEDIA_URL; ?>/js/lib/bootstrap.min.js"></script>
 	</body>
 </html>
